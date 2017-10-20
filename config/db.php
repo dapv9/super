eae1bb46ac2be4e7fb2c4e8bc8ee978bb909cffd
David Parra <david.parra@udea.edu.co>
2017-10-20 18:15:07 -0500

    update db

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'root');//Usuario de tu base de datos
-define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'simple_stock');//Nombre de la base de datos
+define('DB_HOST', 'ec2-107-20-226-93.compute-1.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'ffklwvhysrbcue');//Usuario de tu base de datos
+define('DB_PASS', 'd793e771d947323058987dc718bab324b35777c7d3b56e1dbb5667711605a38d');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'd1kutd0jpqtuse');//Nombre de la base de datos
 ?>
