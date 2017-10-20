efcb2fcb1eed96ffdb58c5e9193ee9ec7113dd4e
David Parra <david.parra@udea.edu.co>
2017-10-20 18:32:30 -0500

    commit

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'ec2-107-20-226-93.compute-1.amazonaws.com:5432');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_HOST', 'ec2-107-20-226-93.compute-1.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'ffklwvhysrbcue');//Usuario de tu base de datos
 define('DB_PASS', 'd793e771d947323058987dc718bab324b35777c7d3b56e1dbb5667711605a38d');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'd1kutd0jpqtuse');//Nombre de la base de datos
